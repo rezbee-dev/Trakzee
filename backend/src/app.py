fab17fffd7fe278022af3c42ce6c451c40ed592e
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2023-05-17 00:50:39 -0400

    Add install file & config

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -8,6 +7,9 @@ app = Flask(__name__)
 
 api = Api(app)
 
+
+# set config
+app.config.from_object('config.DevelopmentConfig')
 
 class Ping(Resource):
     def get(self):
